f3547aa786cd71c5af4a5c4dd3bb92778e8e8c42
Matt Manning <matt.manning@gmail.com>
2011-10-13 00:27:49 -0400

    Add examples from clockwork README

--- clock.rb
+++ clock.rb
@@ -1,3 +1,12 @@
 require 'clockwork'
 include Clockwork
 
+handler do |job|
+  puts "Running #{job}"
+end
+
+every(10.seconds, 'frequent.job')
+every(3.minutes, 'less.frequent.job')
+every(1.hour, 'hourly.job')
+
+every(1.day, 'midnight.job', :at => '00:00')
